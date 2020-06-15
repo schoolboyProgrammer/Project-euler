7a1f3eb63fc7e65f4ed19506496a249093e184de
Monish Singhal <programminggeekishere@gmail.com>
2020-06-15 19:55:01 +0530

    Added files

--- Problem 1/problem1-BruteForce.py
+++ Problem 1/problem1-BruteForce.py
@@ -1,8 +1,10 @@
 # Brute Force approach: Loop from 1 to the last index and check divisibility. If
 # the number is divisible by 5 or 3, print the output
 
-sum_multiples = 0
+sum_multiples = 0 # Stores the sum of numbers
+
 for i in range(1, 1000):
+
   if (i % 3 == 0 or i % 5 == 0):
     sum_multiples += i
 
